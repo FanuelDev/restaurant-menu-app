b7131a995e026be6533af76b0d6824cadd01aa44
Fanuel Israel TOVEY <fanueldev2.0@gmail.com>
2026-05-10 20:10:16 +0000

    Update 1750000015_add_translations_to_content.ts

--- backend/database/migrations/1750000015_add_translations_to_content.ts
+++ backend/database/migrations/1750000015_add_translations_to_content.ts
@@ -3,12 +3,12 @@ import { BaseSchema } from '@adonisjs/lucid/schema'
 export default class extends BaseSchema {
   async up() {
     this.schema.alterTable('categories', (table) => {
-      table.jsonb('name_translations').nullable().defaultTo('{}')
-      table.jsonb('description_translations').nullable().defaultTo('{}')
+      table.jsonb('name_translations').nullable()
+      table.jsonb('description_translations').nullable()
     })
     this.schema.alterTable('menu_items', (table) => {
-      table.jsonb('name_translations').nullable().defaultTo('{}')
-      table.jsonb('description_translations').nullable().defaultTo('{}')
+      table.jsonb('name_translations').nullable()
+      table.jsonb('description_translations').nullable()
     })
   }
 
